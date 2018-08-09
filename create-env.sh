4dd524afd18b0174e9e3897eb9e04de24b5a5082
priyankachennupati <40577708+priyankachennupati@users.noreply.github.com>
2018-08-09 16:24:45 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -5,10 +5,10 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating www.pravallika$i container.."
+    echo "Creating www.priya$i container.."
     sleep 1
-    docker run --name www.pravallika$i -d -it --rm priyankachennupati/real-img bin/bash
-    echo "www.pravallika$i container has been created!"
+    docker run --name www.priya$i -d -it --rm priyankachennupati/real-img bin/bash
+    echo "www.priya$i container has been created!"
 	echo "=============================="
 done
 docker inspect -f '{{.NetworkSettings.Networks.bridge.IPAddress}}'>ip.txt
